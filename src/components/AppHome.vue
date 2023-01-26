cfa7c0121518cc9fbfb169431b5c45cc859bc31f
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-01-26 09:36:33 +0300

    initial commit on new project

---
+++ src/components/AppHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <app-toolbar></app-toolbar>
+  <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import AppToolbar from './AppToolbar.vue'
+import AppFooter from './AppFooter.vue'
+
+export default {
+
+  components: {
+    AppToolbar,
+    AppFooter
+  }
+}
+</script>
---
+++ src/components/AppToolbar.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <v-toolbar dense color="#0F0F0F" dark height="50">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Spotify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Home</v-btn>
+        <v-btn text>Search</v-btn>
+        <v-btn text>Your Library</v-btn>
+        <v-btn text>Login</v-btn>
+        <v-btn text>Sign Up</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</template>
+
